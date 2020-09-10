09ebb4d88a9d65ccafdb9e587bfeebc885590b76
Andrés Nausán <Andrs Nausn@192.168.1.103>
2020-09-10 16:16:45 -0500

    gender

--- src/models/Gender.java
+++ src/models/Gender.java
@@ -1,4 +1,15 @@
 package models;
 
 public enum Gender {
+	MALE('M'),FEMALE('F');
+	
+	private final char gendercharacter;
+	
+	private Gender(char gender) {
+		this.gendercharacter = gender;
+	}
+	
+	public char getGender() {
+		return this.gendercharacter;
+	}
 }
